cdc2f1662d2052b6164e46d5433e9c9074db21a2
AlthafHussainK <althafakm@gmail.com>
2021-03-22 20:03:16 +0530

    async with error

---
+++ fetch_basic/async/index.js
@@ -0,0 +1,25 @@
+//async with error handling
+async function getPost() {
+  const postPromise = await fetch('')
+
+  if(postPromise.ok){
+    const posts = await postPromise.json()
+    let html = ""
+
+    posts.slice(0,5).forEach((post) => {
+      const title = post.title
+      const body = post.body
+      html += `
+        <div class='post'>
+          <h3>${title}</h3>
+          <p>${body}</p>
+        </div>
+      `
+    })
+    document.body.innerHTML = html
+  } else {
+    console.error(`Error: ${postPromise.status}`)
+  }
+}
+
+getPost()
