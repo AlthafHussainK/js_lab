ecc6a2bd7e9f5f1d20f74ec487eaecb98268250d
AlthafHussainK <althafakm@gmail.com>
2021-04-05 13:26:30 +0530

    first commit

---
+++ hello_react/good-M_A_N.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12){
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+
+  return (
+    <h1>Good {timeOfDay}</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
