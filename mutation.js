4b90c8a3f7e277c80629f4f64f999d0383359fb4
AlthafHussainK <althafakm@gmail.com>
2021-04-22 02:50:37 +0530

    upload

---
+++ mutation.js
@@ -0,0 +1,10 @@
+function mutation(arr){
+  let str1 = arr[0].toLowerCase()
+  let str2 = arr[1].toLowerCase()
+
+  for(let i=0; i < str2.length; i++){
+    if (str1.indexOf(str2[i]) < 0) return false
+  }
+
+  return true
+}
