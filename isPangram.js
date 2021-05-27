ac31274a661003fbaf7dd549d10c3409f0aee192
AlthafHussainK <althafakm@gmail.com>
2021-05-27 23:35:49 +0530

    upload

--- isPangram.js
+++ isPangram.js
@@ -1,12 +1,12 @@
 const abc = 'abcdefghijklmnopqrstuvwxyz'
 
 const isPangram = (string) => {
-  // const processedString = [...new Set(string.toLowerCase().split(' ').join(''))].sort().join('')
-  // return abc === processedString
+  const processedString = [...new Set(string.toLowerCase().split(' ').join(''))].sort().join('')
+  return abc === processedString
 
   //regex method
-  const regexMatch = new Set(string.toLowerCase().match(/[a-z]/gi))
-  return regexMatch.size === 26
+  // const regexMatch = new Set(string.toLowerCase().match(/[a-z]/gi))
+  // return regexMatch.size === 26
 }
 
 console.log(isPangram('The quick Brown fox jumps over the lazy DOG'))
