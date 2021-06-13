c0273bb98a974d9f833da9ab9aef71e1abca6a52
AlthafHussainK <althafakm@gmail.com>
2021-06-13 23:15:10 +0530

    Upload

---
+++ os_demo.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+// Platform 
+console.log(os.platform())
+
+//CPU Arch
+console.log(os.arch())
+
+//CPU core info
+console.log(os.cpus())
+
+//Free memory
+console.log(os.freemem())
+
+//Total memory 
+console.log(os.totalmem())
+
+// OS Uptime
+console.log(os.uptime())
